67a8d39df41e513647fa0cde3499d44ab85bc3d0
sizt <982637685@qq.com>
2026-02-18 10:23:34 +0800

    2026年02月18日10:23:32

---
+++ ssitao-code-modulars/ssitao-code-modular-iam/src/main/java/com/ssitao/code/modular/iam/IamApplication.java
@@ -0,0 +1,9 @@
+package com.ssitao.code.modular.iam;
+
+/**
+ * @author sizt
+ * @description: TODO
+ * @date 2026/2/18 09:05
+ */
+public class IamApplication {
+}
---
+++ ssitao-code-modulars/ssitao-code-modular-iam/src/main/java/com/ssitao/code/modular/iam/package-info.java
@@ -0,0 +1,1 @@
+package com.ssitao.code.modular.iam;
